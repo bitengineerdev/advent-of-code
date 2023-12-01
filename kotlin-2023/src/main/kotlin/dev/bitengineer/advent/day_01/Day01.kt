95726f1a95777c570eae972987e83cc02f6f6500
Eric Takemoto <24865872+octothorped@users.noreply.github.com>
2023-12-01 14:44:31 -0800

    minor code cleanup

--- kotlin-2023/src/main/kotlin/dev/bitengineer/advent/day_01/Day01.kt
+++ kotlin-2023/src/main/kotlin/dev/bitengineer/advent/day_01/Day01.kt
@@ -1,42 +1,45 @@
 package dev.bitengineer.advent.day_01
 
 import dev.bitengineer.advent.util.assertEquals
-import dev.bitengineer.advent.util.readDayInput
-import dev.bitengineer.advent.util.readDayTestInput1
-import dev.bitengineer.advent.util.readDayTestInput2
+import dev.bitengineer.advent.util.input
+import dev.bitengineer.advent.util.part1TestFile
+import dev.bitengineer.advent.util.part2TestFile
 
 fun main() {
     val day = "01"
-    val testInput1 = readDayTestInput1(day)
-    assertEquals(142, part1(testInput1))
-    val testInput2 = readDayTestInput2(day)
-    assertEquals(281, part2(testInput2))
+    part1TestFile(day).useLines { input ->
+        assertEquals(142, part1(input))
+    }
+    part2TestFile(day).useLines { input ->
+        assertEquals(281, part2(input))
+    }
 
-    val input = readDayInput(day)
-    println("Part 1: ${part1(input)}")
-    println("Part 2: ${part2(input)}")
+    input(day).useLines { input ->
+        println("Part 1: ${part1(input)}")
+    }
+    input(day).useLines { input ->
+        println("Part 2: ${part2(input)}")
+    }
 }
 
-fun part1(input: List<String>): Int = input.sumOf { it.findFirstAndLastNumberChar() }
+fun part1(input: Sequence<String>): Int = input.sumOf { it.calibrationValueByDigit() }
 
-fun part2(input: List<String>): Int = input.sumOf { it.findFirstAndLastNumberCharOrWord() }
+fun part2(input: Sequence<String>): Int = input.sumOf { it.calibrationValueByWordOrNumber() }
 
-private fun String.findFirstAndLastNumberChar(): Int {
-    val firstDigit = first { it.isDigit() }
-    val lastDigit = last { it.isDigit() }
-    return formNumber(firstDigit.toString(), lastDigit.toString())
+private fun String.calibrationValueByDigit(): Int {
+    val firstDigit = first { it.isDigit() }.digitToInt()
+    val lastDigit = last { it.isDigit() }.digitToInt()
+    return formNumber(firstDigit, lastDigit)
 }
 
-private fun String.findFirstAndLastNumberCharOrWord(): Int {
+private fun String.calibrationValueByWordOrNumber(): Int {
     val firstDigit = findNumber(0, reversed = false) ?: error("No first digit found in $this")
     val lastDigit = reversed().findNumber(0, reversed = true) ?: error("No last digit found in $this")
 
-    val number = formNumber(firstDigit.toString(), lastDigit.toString())
-    println("Number: $number ($firstDigit, $lastDigit) in $this")
-    return number
+    return formNumber(firstDigit, lastDigit)
 }
 
-private fun formNumber(first: String, last: String): Int = (first + last).toInt()
+private fun formNumber(first: Int, second: Int): Int = (first * 10) + second
 
 private fun String.findNumber(startPos: Int, reversed: Boolean): Int? {
     if (this[startPos].isDigit()) return this[startPos].toString().toInt()
@@ -46,7 +49,7 @@ private fun String.findNumber(startPos: Int, reversed: Boolean): Int? {
             if (c.isDigit()) return@forEachIndexed
             val wordNumber = subWord.substring(0, index + 1)
                 .let { if (reversed) it.reversed() else it }
-                .asNumberWordOrNull()
+                .let { numberWords[it] }
             if (wordNumber != null) {
                 return wordNumber
             }
@@ -56,16 +59,14 @@ private fun String.findNumber(startPos: Int, reversed: Boolean): Int? {
     return this.findNumber(startPos + 1, reversed)
 }
 
-private fun String.asNumberWordOrNull(): Int? =
-    when (this) {
-        "one" -> 1
-        "two" -> 2
-        "three" -> 3
-        "four" -> 4
-        "five" -> 5
-        "six" -> 6
-        "seven" -> 7
-        "eight" -> 8
-        "nine" -> 9
-        else -> null
-    }
+private val numberWords = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9
+)
--- kotlin-2023/src/main/kotlin/dev/bitengineer/advent/util/Utils.kt
+++ kotlin-2023/src/main/kotlin/dev/bitengineer/advent/util/Utils.kt
@@ -6,11 +6,11 @@ import java.io.File
  * Reads lines from the given input txt file.
  */
 const val PROJECT = "kotlin-2023"
-fun readInput(name: String) = File("$PROJECT/src/main/kotlin/dev/bitengineer/advent", name).readLines()
+fun readInput(name: String) = File("$PROJECT/src/main/kotlin/dev/bitengineer/advent", name)
 
-fun readDayTestInput1(day: String) = readInput("day_$day/test_input_1.txt")
-fun readDayTestInput2(day: String) = readInput("day_$day/test_input_2.txt")
-fun readDayInput(day: String) = readInput("day_$day/input.txt")
+fun part1TestFile(day: String) = readInput("day_$day/test_input_1.txt")
+fun part2TestFile(day: String) = readInput("day_$day/test_input_2.txt")
+fun input(day: String) = readInput("day_$day/input.txt")
 
 fun assertEquals(expected: Any, actual: Any) {
     if (expected != actual) {
