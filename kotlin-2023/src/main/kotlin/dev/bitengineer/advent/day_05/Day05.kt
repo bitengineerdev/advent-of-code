4601b897d53bacf4f8f0b7f0e714174e5ee06977
Eric Takemoto <24865872+octothorped@users.noreply.github.com>
2023-12-07 17:03:29 -0800

    Day 5 full solution

--- kotlin-2023/src/main/kotlin/dev/bitengineer/advent/day_05/Day05.kt
+++ kotlin-2023/src/main/kotlin/dev/bitengineer/advent/day_05/Day05.kt
@@ -7,39 +7,79 @@ import dev.bitengineer.advent.util.part1TestFile
 fun main() {
     val day = "05"
     assertEquals(35L, part1(part1TestFile(day).readText()))
-    // part1TestFile(day).useLines { input ->
-    //     assertEquals(30, part2(input))
-    // }
+    val resultPart1 = part1(input(day).readText())
+    assertEquals(323142486L, resultPart1)
+    println("Part 1: $resultPart1")
 
-    println("Part 1: ${part1(input(day).readText())}")
-    // input(day).useLines { input ->
-    //     println("Part 2: ${part2(input)}")
-    // }
+    assertEquals(46L, part2(part1TestFile(day).readText()))
+    val resultPart2 = part2(input(day).readText())
+    println("Part 2: $resultPart2")
 }
 
 fun part1(input: String): Long {
-    val blocks = input.split("\n\n")
-    val seeds = blocks.first().split(" ").mapNotNull { it.toLongOrNull() }
-    val categoryMappers = blocks.drop(1).map { CategoryMapper.from(it.split("\n")) }
-        .associateBy { it.source }
-
-    return seeds.minOf { categoryMappers.findLocationFromSeed(it) }
+    val (seeds, categoryMappers) = parse(input, seedsInRanges = false)
+    return seeds.flatMap { it.findLocation(categoryMappers) }.minOf { it.first }
 }
 
-fun part2(input: List<String>): Int = TODO()
+fun part2(input: String): Long {
+    val (seeds, categoryMappers) = parse(input, seedsInRanges = true)
+    return seeds.flatMap { it.findLocation(categoryMappers) }.minOf { it.first }
+}
+
+private fun LongRange.findLocation(categoryMappers: List<CategoryMapper>): List<LongRange> =
+    categoryMappers.fold(listOf(this)) { ranges, categoryMapper ->
+        ranges.flatMap { range ->
+            categoryMapper.mappers.mapNotNull { mapper -> range.mapTo(mapper) }
+                .takeIf { it.isNotEmpty() }
+                ?: listOf(range)
+        }
+    }
+
+private fun LongRange.mapTo(rangeMapper: RangeMapper): LongRange? {
+    return if (rangeMapper.range.first >= this.first && rangeMapper.range.last <= this.last) {
+        // applied range mapper is fully contained in the source range
+        rangeMapper.range.shift(rangeMapper.mappedValue)
+    } else if (this.first >= rangeMapper.range.first && this.last <= rangeMapper.range.last) {
+        // applied range mapper fully contains other range mapper
+        this.shift(rangeMapper.mappedValue)
+    } else if (rangeMapper.range.first < this.last && rangeMapper.range.last > this.last) {
+        // applied range mapper overlaps on the left side
+        (rangeMapper.range.first..this.last).shift(rangeMapper.mappedValue)
+    } else if (rangeMapper.range.first < this.first && rangeMapper.range.last > this.first) {
+        // applied range mapper overlaps on the right side
+        (this.first..rangeMapper.range.last).shift(rangeMapper.mappedValue)
+    } else {
+        // applied range mapper is completely outside of the source range
+        null
+    }
+}
+
+private fun LongRange.shift(shift: Long): LongRange = (this.first + shift)..(this.last + shift)
+
+private fun parse(input: String, seedsInRanges: Boolean): Almanac {
+    val blocks = input.split("\n\n")
+    val seedBlock = blocks.first().split(" ").mapNotNull { it.toLongOrNull() }
+    val seeds = if (seedsInRanges) {
+        seedBlock.chunked(2).map { it.first().until(it.first() + it.last()) }
+    } else {
+        seedBlock.map { it..it }
+    }
+    val rangeMappers = blocks.drop(1).map { CategoryMapper.from(it) }
+
+    return Almanac(seeds, rangeMappers)
+}
+
+private data class Almanac(
+    val seeds: List<LongRange>,
+    val categoryMappers: List<CategoryMapper>
+)
 
 private data class CategoryMapper(
-    val source: String,
-    val destination: String,
-    val mappedValues: List<RangeMapper>
+    val mappers: List<RangeMapper>
 ) {
-    fun map(value: Long): Long = value + (mappedValues.firstOrNull { it.range.contains(value) }?.mappedValue ?: 0L)
-
     companion object {
-        fun from(block: List<String>): CategoryMapper {
-            val (source, destination) = block.first().removeSuffix(" map:").split("-to-", " ", limit = 2)
-            val mappers = block.drop(1).map { RangeMapper.from(it) }
-            return CategoryMapper(source, destination, mappers)
+        fun from(block: String): CategoryMapper {
+            return CategoryMapper(block.split("\n").drop(1).map { RangeMapper.from(it) })
         }
     }
 }
@@ -52,20 +92,6 @@ private data class RangeMapper(
         fun from(input: String): RangeMapper {
             val (destination, source, range) = input.split(" ", limit = 3).map { it.toLong() }
             return RangeMapper(source.until(source + range), destination - source)
-        }
-    }
-}
-
-private fun Map<String, CategoryMapper>.findLocationFromSeed(seed: Long): Long {
-    var category = this["seed"] ?: error("No category found for seed")
-    var value = seed
-    while(true) {
-        println("${category.source} $value -> ${category.destination} -> ${category.map(value)}")
-        value = category.map(value)
-        if (category.destination == "location") {
-            return value
-        } else {
-            category = this[category.destination] ?: error("No category found for ${category.destination}")
         }
     }
 }
